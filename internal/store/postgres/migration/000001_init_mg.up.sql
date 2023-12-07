d3b131379df480d3c15c56be135b1a5fc1c106d5
g0dm0d <ivan.berlin06@gmail.com>
2023-12-07 20:27:09 +0300

    add migration

---
+++ internal/store/postgres/migration/000001_init_mg.up.sql
@@ -0,0 +1,23 @@
+-- users db
+
+CREATE TABLE IF NOT EXISTS users (
+    id serial PRIMARY KEY,
+    email TEXT UNIQUE,
+    password_hash TEXT,
+    first_name TEXT,
+    last_name TEXT,
+    role_id integer REFERENCES roles (id),
+    created_at timestamp DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS roles (
+    id serial PRIMARY KEY,
+    name TEXT,
+);
+
+CREATE TABLE IF NOT EXISTS sessions (
+    id serial PRIMARY KEY,
+    session TEXT UNIQUE,
+    expires_at timestamp,
+    created_at timestamp DEFAULT NOW()
+);
