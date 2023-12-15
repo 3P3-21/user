abee1ef45c7612463d36d155c542852cd1c0e0bd
M1Proper <55317997+M1Proper@users.noreply.github.com>
2023-12-15 15:07:46 +0300

    Update 000001_init_mg.down.sql

--- internal/store/postgres/migration/000001_init_mg.down.sql
+++ internal/store/postgres/migration/000001_init_mg.down.sql
@@ -2,3 +2,6 @@
 DROP TABLE IF EXISTS sessions;
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS users;
+CREATE USER 'username'@'localhost' IDENTIFIED BY 'password';
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON database_name.* TO 'username'@'localhost';
